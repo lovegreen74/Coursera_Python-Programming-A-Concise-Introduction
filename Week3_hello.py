5beb63189ac0a0e67bad27a0c912b9264e4ad0cd
Nq_nnhu <75362571+lovegreen74@users.noreply.github.com>
2020-12-09 23:18:01 +0100

    Add files via upload

---
+++ Week3_hello.py
@@ -0,0 +1,24 @@
+# - helloworld.py *- coding: utf-8 -*-
+"""
+This program simply prints the phrase "Hello, World!"
+
+@author: wboyd
+"""
+
+print("Hello, World!")
+print("Goodbye")
+
+"""
+Running Python Program (Mac)
+
+1. Terminal
+2. Highlight current working directory -> Cmd+C
+3. cd + space + Cmd+V -> Return
+Note: if the result is cd:too many arguments, use quotes or escape the space
+ ex: cd "new folder"
+     cd new\ folder
+4. pwd: print working directory ( if u want to check if you're there')
+5. python folder_name*.py
+
+"""
+
